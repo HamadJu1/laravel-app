f83aa6f1b66f957237e87515588ebfdaf0e3209b
Hamad <133687688+HamadJu1@users.noreply.github.com>
2024-12-08 16:20:13 +0300

    just editing

--- resources/views/login.blade.php
+++
@@ -1,6 +0,0 @@
-<form action="{{ route('login') }}" method="POST">
-    @csrf
-    <input type="text" name="username" placeholder="Username">
-    <input type="password" name="password" placeholder="Password">
-    <button type="submit">Login</button>
-</form>
