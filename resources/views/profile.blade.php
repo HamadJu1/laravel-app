5a178426b641f1e82741f4cff7163f638bd7f15a
Hamad <133687688+HamadJu1@users.noreply.github.com>
2024-12-08 17:10:24 +0300

    just editing

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,3 +1,26 @@
-<h1>Profile</h1>
-<p>Username: {{ $username }}</p>
-<p>Password: {{ $password }}</p>
+```blade
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h1 class="mb-4">Profile</h1>
+    <div class="card">
+        <div class="card-body">
+            <div class="row mb-3">
+                <div class="col-md-4">
+                    <img src="{{ auth()->user()->avatar }}" alt="Profile Picture" class="rounded-circle img-fluid">
+                </div>
+                <div class="col-md-8">
+                    <h2>{{ auth()->user()->name }}</h2>
+                    <p>Email: {{ auth()->user()->email }}</p>
+                    <p>Joined: {{ auth()->user()->created_at->format('M d, Y') }}</p>
+                </div>
+            </div>
+            <div class="text-end">
+                <a href="{{ route('profile.edit') }}" class="btn btn-primary">Edit Profile</a>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+```
