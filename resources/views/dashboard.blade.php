ff14ef562e6072621877b52616ae78802cf112ac
Hamad <133687688+HamadJu1@users.noreply.github.com>
2024-12-09 10:03:40 +0300

    just editing

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,76 +1,64 @@
 ```blade
+{{-- dashboard.blade.php --}}
 @extends('layouts.app')
 
 @section('content')
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-            <h1>Dashboard</h1>
-        </div>
-    </div>
+            <div class="card">
+                <div class="card-header">
+                    <h3>Dashboard</h3>
+                </div>
+                <div class="card-body">
+                    <div class="row">
+                        <!-- Example Widget -->
+                        <div class="col-md-4">
+                            <div class="card text-white bg-primary mb-3">
+                                <div class="card-header">Users</div>
+                                <div class="card-body">
+                                    <h5 class="card-title">Total Users</h5>
+                                    <p class="card-text">{{ $usersCount }}</p>
+                                </div>
+                            </div>
+                        </div>
 
-    <div class="row mt-4">
-        <div class="col-md-4">
-            <div class="card">
-                <div class="card-header">
-                    <h5>Total Users</h5>
-                </div>
-                <div class="card-body">
-                    <h3>{{ $totalUsers }}</h3>
-                </div>
-            </div>
-        </div>
-        
-        <div class="col-md-4">
-            <div class="card">
-                <div class="card-header">
-                    <h5>Total Orders</h5>
-                </div>
-                <div class="card-body">
-                    <h3>{{ $totalOrders }}</h3>
-                </div>
-            </div>
-        </div>
+                        <!-- Example Widget -->
+                        <div class="col-md-4">
+                            <div class="card text-white bg-success mb-3">
+                                <div class="card-header">Posts</div>
+                                <div class="card-body">
+                                    <h5 class="card-title">Total Posts</h5>
+                                    <p class="card-text">{{ $postsCount }}</p>
+                                </div>
+                            </div>
+                        </div>
 
-        <div class="col-md-4">
-            <div class="card">
-                <div class="card-header">
-                    <h5>Total Revenue</h5>
-                </div>
-                <div class="card-body">
-                    <h3>${{ $totalRevenue }}</h3>
-                </div>
-            </div>
-        </div>
-    </div>
+                        <!-- Example Widget -->
+                        <div class="col-md-4">
+                            <div class="card text-white bg-warning mb-3">
+                                <div class="card-header">Comments</div>
+                                <div class="card-body">
+                                    <h5 class="card-title">Total Comments</h5>
+                                    <p class="card-text">{{ $commentsCount }}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
-    <div class="row mt-4">
-        <div class="col-md-6">
-            <div class="card">
-                <div class="card-header">
-                    <h5>Recent Users</h5>
-                </div>
-                <div class="card-body">
-                    <ul class="list-group">
-                        @foreach($recentUsers as $user)
-                            <li class="list-group-item">{{ $user->name }} - {{ $user->email }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-md-6">
-            <div class="card">
-                <div class="card-header">
-                    <h5>Recent Orders</h5>
-                </div>
-                <div class="card-body">
-                    <ul class="list-group">
-                        @foreach($recentOrders as $order)
-                            <li class="list-group-item">Order #{{ $order->id }} - ${{ $order->amount }}</li>
-                        @endforeach
-                    </ul>
+                    <!-- Recent Entries -->
+                    <div class="row mt-4">
+                        <div class="col-md-12">
+                            <h4>Recent Activities</h4>
+                            <ul class="list-group">
+                                @foreach ($recentActivities as $activity)
+                                    <li class="list-group-item">
+                                        {{ $activity }}
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
